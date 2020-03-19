9d852d894a18b6f6b1c93b7854323e9c5ecf5e15
Vijayanand Gajapathy <Vijayanand.pg6@gmail.com>
2020-03-20 00:51:00 +0530

    Adding answer validations

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -85,6 +85,21 @@ class  PlayGameIntentHandler(AbstractRequestHandler):
                             .speak(speak_output)
                             .response
                     )
+			speak_output += " Now, Player 1 at position " + str(attr["Player1"]) + " and Player 2 at position " + str(attr["Player2"])  + ". "
+			if(attr.get("PlayerState") == "VALIDATEANSWER"):
+                # 3. validate user's VALIDATEANSWER
+                currentAnswer = str(slots['QuizResponse'].value)
+                answerOption = ""
+                if(str(attr.get("CorrectOptionID")) == currentAnswer):
+					if(attr.get("Player2") >= MAXVALUE):
+                        speak_output += "Player 2 wins. Congratulations. " + alexaOP_applauce + "Thank you for playing this game."
+                        attr["GameState"] = "INITIALIZE"
+                        attr["PlayerState"] = "ROLLDIEANDQUESTION"
+                        return (
+                            handler_input.response_builder
+                                .speak(speak_output)
+                                .response
+                        )
         return (
             handler_input.response_builder
                 .speak(speak_output)
