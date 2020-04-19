2148532028ab1e5ffaad22d6c112a984908e7385
Vijayanand Gajapathy <Vijayanand.pg6@gmail.com>
2020-04-20 00:55:42 +0530

    Added seperate configurable variables

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -15,6 +15,54 @@ from random import randrange
 from ask_sdk_model import Response
 
 
+# Game States
+INITIALIZE = "INITIALIZE"
+START = "START"
+PLAYERTURNS = "PLAYERTURNS"
+GAMEOVER = "GAMEOVER"  
+
+# Player States
+ROLLDIEANDQUESTION = "ROLLDIEANDQUESTION"
+VALIDATEANSWER = "VALIDATEANSWER"
+INCREASE = "INCREASE"
+REDUCE = "REDUCE"
+
+# currentPlayer = 0
+snakes = {
+    8: 4,
+    18: 1,
+    26: 10,
+    39: 5,
+    51: 6,
+    54: 36,
+    56: 1,
+    60: 23,
+    75: 28,
+    83: 45,
+    85: 59,
+    90: 48,
+    92: 25,
+    97: 87,
+    99: 63
+}
+
+ladders = {
+    3: 20,
+    6: 14,
+    11: 28,
+    15: 34,
+    17: 74,
+    22: 37,
+    38: 59,
+    49: 67,
+    57: 76,
+    61: 78,
+    73: 86,
+    81: 98,
+    88: 91
+}
+
+
 class LaunchRequestHandler(AbstractRequestHandler):
     """Handler for Skill Launch."""
     def can_handle(self, handler_input):
